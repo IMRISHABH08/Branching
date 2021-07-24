0ba4de2bd2695ff4a2da9b8a243ff8059134e3ab
texture <rishabh@pietechsolution.com>
2021-07-24 21:03:46 +0530

    Json file added"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:catalog_app/models/itemclass.dart';
 import 'package:catalog_app/widgets/itemview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,8 +13,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    loading_data();
+  }
+
+  void loading_data() async {
+    Future.delayed(Duration(seconds: 2));
+    final jsonString =
+        await rootBundle.loadString("assets/catalogdata/data.json");
+    final decodedData = jsonDecode(jsonString);
+    final listData = decodedData["products"];
+    CatalogData.item =
+        List.from(listData).map<Items>((e) => Items.fromMap(e)).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List duplicate = List.generate(10, (index) => CatalogData.item[0]);
+    // List duplicate = List.generate(10, (index) => CatalogData.item[0]);
     return Scaffold(
       drawer: MyDrawer(),
       appBar: AppBar(
@@ -19,13 +38,14 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-            itemCount: duplicate.length,
+        child:(CatalogData.item!=null && CatalogData.item.isNotEmpty)? ListView.builder(
+            itemCount: CatalogData.item.length,
             itemBuilder: (context, index) {
               return ItemView(
-                item: duplicate[index],
+                item: CatalogData.item[index],
               );
-            }),
+            })
+            :Center(child: CircularProgressIndicator()),
       ),
     );
   }
